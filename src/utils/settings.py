751257a58b607795c631eb4703e1d01f1fb4fc3e
somath <thmartinezv@unal.edu.co>
2026-03-09 12:24:23 -0500

    add db credentials and api kesys

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -5,3 +5,19 @@ import os
 load_dotenv()
 
 ROOT = Path(__file__).resolve().parent.parent.parent
+
+# Credenciales de edubot ----------------
+
+EDUBOTDB_USER=os.getenv("EDUBOTDB_USER")
+EDUBOTDB_PASS=os.getenv("EDUBOTDB_PASS")
+EDUBOTDB_HOST=os.getenv("EDUBOTDB_HOST")
+EDUBOTDB_PORT=os.getenv("EDUBOTDB_PORT")
+EDUBOTDB_NAME=os.getenv("EDUBOTDB_NAME")
+
+
+# api keys ----------------
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+DEEPSEEK_API_KEY=os.getenv("DEEPSEEK_API_KEY")
+
