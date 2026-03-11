66012c979416c5b1380f093bcaaf522607e74701
somath <thmartinezv@unal.edu.co>
2026-03-10 20:37:00 -0500

    add schemas

--- src/api/v1/schemas/users.py
+++ src/api/v1/schemas/users.py
@@ -1,10 +1,22 @@
 from pydantic import BaseModel
 
+
 class NewUser(BaseModel):
-    name : str
-    email : str
-    password : str
+    name: str
+    email: str
+    password: str
+
 
 class NewUserResponse(BaseModel):
-    message : str
+    message: str
 
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    user_id: int
+    name: str
+    email: str
